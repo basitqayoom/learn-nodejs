0375220b410f34c067ced4e9364362b0a066b367
Basit Qayoom <basitqayoomchowdhary@gmail.com>
2024-06-13 00:16:53 +0530

    How URL works

--- create-server/index.js
+++ create-server/index.js
@@ -1,18 +1,25 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") res.end();
     const date = Date.now()
-    const log = `${date.toString()}: New request recieved from ${req.socket.remoteAddress} at ${req.url}\n`;
-
-    switch (req.url) {
+    const log = `${date.toString()}: New request recieved at ${req.url}\n`;
+    const _url = url.parse(req.url, true);
+    // console.log(_url)
+    switch (_url.pathname) {
         case "/": res.end("Home Page")
             break;
-        case "/about": res.end("About Page")
+        case "/about":
+            const username = _url.query.myName;
+            res.end(`Hi ${username}`)
             break;
+        case "/search":
+            const search = _url.query.search_query;
+            res.end(`Here are some results for ${search}`)
         default:
             res.end("404 Not Found")
-
     }
 
     fs.appendFile('log.txt', log, (err, data) => {
